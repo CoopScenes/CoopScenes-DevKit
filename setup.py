606fb37593b9c2ad5eb1fbaad3d2ecc020384c06
MarcelVHsE <93249016+AlexanderBMN@users.noreply.github.com>
2024-07-05 00:56:42 +0200

    updated requirements

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='ameise',
-    version='0.5',
+    version='0.5.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
