fixed depth calculation, updated projection image input parameters

aeifdataset/develop/tests.py
@@ -127,30 +127,32 @@ def format_to_4x4_matrix(line_content: str) -> np.ndarray:
 
 if __name__ == '__main__':
     # save_dir = '/mnt/dataset/anonymisation/validation/27_09_seq_1/png'
-    # dataset = ad.Dataloader("/mnt/hot_data/dataset/seq_1")
-    frame_num = 1
-    assert frame_num >= 1
-    frame = ad.DataRecord('/mnt/hot_data/dataset/aeif/seq_4/id02322_2024-10-12_11-48-23.4mse')[
-        frame_num]
-    frame2 = ad.DataRecord('/mnt/hot_data/dataset/aeif/seq_4/id02322_2024-10-12_11-48-23.4mse')[
-        frame_num + 1]
-    frame3 = ad.DataRecord('/mnt/hot_data/dataset/aeif/seq_4/id02322_2024-10-12_11-48-23.4mse')[
-        frame_num + 2]
-    tf = ad.get_transformation(frame.vehicle.info)
-    tf2 = ad.get_transformation(frame2.vehicle.info)
-    tf3 = ad.get_transformation(frame.vehicle.lidars.LEFT)
+    # dataset = ad.Dataloader("/mnt/hot_data/dataset/seq_6")
 
-    tf4 = tf3.combine_transformation(tf.combine_transformation(tf2.invert_transformation())).combine_transformation(
-        tf3.invert_transformation())
+    # frame = ad.DataRecord('/mnt/hot_data/dataset/aeif/seq_2/id03954_2024-10-05_12-13-56.4mse')[50]
+    for frame in ad.DataRecord('/mnt/hot_data/dataset/aeif/seq_6/id01442_2024-10-29_09-08-17.4mse')[5:25]:
+        stereo_left = frame.vehicle.cameras.STEREO_LEFT
+        stereo_right = frame.vehicle.cameras.STEREO_RIGHT
 
-    lidar_points = frame.vehicle.lidars.LEFT
+        # Retrieve disparity map from stereo images
+        disparity_map = ad.get_disparity_map(stereo_left, stereo_right)
 
-    lidar_points.info.set_motion_transform(tf4.mtx)
+        # Generate a depth map from stereo images
+        depth_map = ad.get_depth_map(stereo_left, stereo_right)
 
-    image = ad.get_projection_img(frame.vehicle.cameras.STEREO_LEFT,
-                                  lidar_points)
+        # Alternatively, convert a disparity map to a depth map using camera parameters
+        depth_map = ad.disparity_to_depth(disparity_map, stereo_right.info)
 
-    image.show()
+        image = frame.vehicle.cameras.STEREO_LEFT
+
+        proj_img = ad.get_projection_img(image, (frame.vehicle, (64, 200, 200)),
+                                         (frame.tower, (219, 48, 130)))
+        proj_img_2 = ad.get_projection_img(image, frame, min_range=0, max_range=35)
+
+        # ad.show_points((frame.vehicle, (64, 200, 200)), (frame.tower, (219, 48, 130)))
+        speed2 = np.linalg.norm(frame.vehicle.DYNAMICS.velocity[0].linear_velocity) * 3.6
+        print(speed2)
+        proj_img.show()
 
     """
     def _get_timestamps(points):

aeifdataset/utils/image.py
@@ -18,6 +18,7 @@ Functions:
 from typing import Optional, Tuple, Union
 from PIL import Image as PilImage
 from aeifdataset.data import CameraInformation, Camera, Image
+from aeifdataset.utils import Transformation
 import numpy as np
 import cv2
 
@@ -152,7 +153,8 @@ def disparity_to_depth(disparity_map: np.ndarray, camera_info: Union[Camera, Cam
         camera_info = camera_info
 
     focal_length = camera_info.camera_mtx[0][0]
-    baseline = abs(camera_info.stereo_transform.translation[0])
+    stereo_tf = Transformation('stereo_left', 'stereo_right', camera_info.stereo_transform)
+    baseline = abs(stereo_tf.translation[0])
 
     with np.errstate(divide='ignore'):
         depth_map = np.where(disparity_map > 0, (focal_length * baseline) / disparity_map, np.inf)

aeifdataset/utils/transformation.py
@@ -256,12 +256,10 @@ def get_deskewed_points(data: Union[Lidar, Tuple[Points, LidarInformation]]) -> 
             preprocessor = get_preprocessor(k_config)
 
             points_ts = points['t']
-            # normalize
+
             timestamps = (points_ts - points_ts.min()) / (points_ts.max() - points_ts.min())
             points_xyz = np.stack((points['x'], points['y'], points['z']), axis=-1)
 
-            # apply motion compensation, invert timestamps since the algorithm "shifts" the points to the end of the
-            # motion. We compensate reverse to maintain frame integrity.
             points_deskewed = preprocessor.preprocess(points_xyz, 1 - timestamps, lidar_info.motion_transform)
             fields = ['intensity', 't', 'reflectivity', 'ring', 'ambient']
             points_additional = np.stack([points[field] for field in fields], axis=-1)

aeifdataset/utils/visualisation.py
@@ -111,19 +111,27 @@ def plot_points_on_image(image: PilImage, points: np.ndarray, points_3d: np.ndar
 
 
 def get_projection_img(camera: Camera,
-                       *lidars: Union[Lidar, Tuple[Lidar, Optional[Union[str, Tuple[int, int, int]]]]],
-                       cmap_name: str = "Spectral", radius: float = 2.5,
-                       min_range: Optional[float] = 4, max_range: Optional[float] = 50,
+                       *args: Union[
+                           Lidar,
+                           Frame,
+                           Vehicle,
+                           Tower,
+                           Tuple[Union[Lidar, Frame, Vehicle, Tower], Optional[Union[str, Tuple[int, int, int]]]]],
+                       cmap_name: str = "Spectral",
+                       radius: float = 2.5,
+                       min_range: Optional[float] = 4,
+                       max_range: Optional[float] = 50,
                        opacity: float = 0.6,
                        vehicle_info: Optional[VehicleInformation] = None) -> PilImage:
     """Generate an image with LiDAR points projected onto a camera image.
 
-    Projects LiDAR points onto a camera image with optional colormap-based or static coloring.
+    Projects LiDAR points from various sources (LiDARs, Frame, Vehicle, Tower) onto a camera image
+    with optional colormap-based or static coloring.
 
     Args:
         camera (Camera): The camera onto which the LiDAR points are projected.
-        *lidars (Union[Lidar, Tuple[Lidar, Optional[Union[str, Tuple[int, int, int]]]]]): One or more LiDAR objects,
-            optionally paired with a static color.
+        *args: One or more sources of LiDAR points, such as Frame, Vehicle, Tower, Lidar, or
+            tuples of (source, optional color).
         cmap_name (str): The name of the colormap for dynamic coloring. Defaults to "Spectral".
         radius (float): The radius of the plotted points. Defaults to 2.5.
         min_range (Optional[float]): Minimum range value for normalization. Defaults to 4.
@@ -137,16 +145,44 @@ def get_projection_img(camera: Camera,
     proj_img = camera.image.image.copy()
 
     lidar_list = []
-    for lidar in lidars:
-        if isinstance(lidar, Lidar):
-            lidar_list.append((lidar, None))
-        elif isinstance(lidar, tuple) and isinstance(lidar[0], Lidar):
-            lidar_list.append(lidar)
+
+    for source in args:
+        if isinstance(source, Lidar):
+            lidar_list.append((source, None))
+        elif isinstance(source, tuple) and isinstance(source[0], (Lidar, Frame, Vehicle, Tower)):
+            obj, static_color = source
+            if isinstance(obj, Frame):
+                vehicle_info = vehicle_info or obj.vehicle.info
+                for _, lidar in (*obj.vehicle.lidars, *obj.tower.lidars):
+                    lidar_list.append((lidar, static_color))
+            elif isinstance(obj, Vehicle):
+                vehicle_info = vehicle_info or obj.info
+                for _, lidar in obj.lidars:
+                    lidar_list.append((lidar, static_color))
+            elif isinstance(obj, Tower):
+                for _, lidar in obj.lidars:
+                    lidar_list.append((lidar, static_color))
+            elif isinstance(obj, Lidar):
+                lidar_list.append((obj, static_color))
+        elif isinstance(source, Frame):
+            vehicle_info = vehicle_info or source.vehicle.info
+            for _, lidar in (*source.vehicle.lidars, *source.tower.lidars):
+                lidar_list.append((lidar, None))
+        elif isinstance(source, Vehicle):
+            vehicle_info = vehicle_info or source.info
+            for _, lidar in source.lidars:
+                lidar_list.append((lidar, None))
+        elif isinstance(source, Tower):
+            for _, lidar in source.lidars:
+                lidar_list.append((lidar, None))
         else:
-            raise ValueError("Each argument must be either a Lidar object or a tuple of (Lidar, optional color).")
+            raise ValueError(
+                "Each source must be a Lidar, Frame, Vehicle, Tower, or a tuple of (source, optional color)."
+            )
 
     for lidar, static_color in lidar_list:
         pts, proj = get_projection(lidar, camera, vehicle_info)
+        pts = transform_points_to_origin((pts, lidar.info), vehicle_info=vehicle_info)
         proj_img = plot_points_on_image(proj_img, proj, pts, static_color=static_color, cmap_name=cmap_name,
                                         radius=radius, min_range=min_range, max_range=max_range, opacity=opacity)
 